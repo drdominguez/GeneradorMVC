9129a43808c6aeaa37dee9fe1830bb82b48dfe9e
daniel <drdominguez@esei.uvigo.es>
2017-06-01 19:10:44 +0200

    Añadido head al generar la vista add

--- GenerarVistas.php
+++ GenerarVistas.php
@@ -56,6 +56,14 @@ function crearADD($tabla){
                 
     function render(){ 
     
+?>
+	<head><link rel="stylesheet" href="../Styles/styles.css" type="text/css" media="screen" />
+		<script type="text/javascript" src="../js/<?php  echo $_SESSION[\'IDIOMA\']?>_validate.js"></script></head>
+	<div>
+		<p>
+			<h2>
+<?php
+    
     include \'../Locates/Strings_\'.$_SESSION[\'IDIOMA\'].\'.php\';           
         include \'../Functions/' . strtoupper($tabla) . '_DefForm.php\';
         
