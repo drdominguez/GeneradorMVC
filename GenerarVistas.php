368219c8d8df2a761657f11cd6deac3b736c3dd3
Adrián <asfarinhas@esei.uvigo.es>
2017-06-05 21:09:52 +0200

    delete, add y show all estan. EDIT tengo que mirar como hacer con el create form

--- GenerarVistas.php
+++ GenerarVistas.php
@@ -29,9 +29,10 @@ foreach($arrayTablas as $tabla){//Recorremos el array con las vistas
     crearADD($tabla);
     crearSHOWALL($tabla);
     crearDELETE($tabla);
-    crearEDIT($tabla);
+    
   /*  crearSEARCH($tabla);
-    crearSHOWCURRENT($tabla);*/
+    crearSHOWCURRENT($tabla);
+    crearEDIT($tabla);*/
 }
 echo "Vistas creadas";
 
